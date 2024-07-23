8b4fe1c71151c8049503df64e367f6c914837d6d
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-07-22 20:29:46 -0700

    integrated unit tests for tires

--- ts-rental-fleet-management/src/__tests__/models/battery/SpindlerBattery.test.ts
+++ ts-rental-fleet-management/src/__tests__/models/battery/SpindlerBattery.test.ts
@@ -1,21 +1,22 @@
 import { assertEquals } from "jsr:@std/assert/equals";
 import { SpindlerBattery } from "../../../models/battery/SpindlerBattery.ts";
 
-Deno.test("SpindlerBattery - needs service after 2 years", () => {
+Deno.test("SpindlerBattery - needs service after 3 years", () => {
   const battery = new SpindlerBattery();
   const currentDate = new Date();
   const lastServiceDate = new Date(currentDate);
-  lastServiceDate.setFullYear(lastServiceDate.getFullYear() - 2);
+  lastServiceDate.setFullYear(lastServiceDate.getFullYear() - 3);
   lastServiceDate.setDate(lastServiceDate.getDate() - 1);
   
   assertEquals(battery.needsService(lastServiceDate), true);
 });
 
-Deno.test("SpindlerBattery - doesn't need service before 2 years", () => {
+Deno.test("SpindlerBattery - doesn't need service before 3 years", () => {
   const battery = new SpindlerBattery();
   const currentDate = new Date();
   const lastServiceDate = new Date(currentDate);
-  lastServiceDate.setFullYear(lastServiceDate.getFullYear() - 1);
+  lastServiceDate.setFullYear(lastServiceDate.getFullYear() - 2);
+  lastServiceDate.setDate(lastServiceDate.getDate() + 364);
   
   assertEquals(battery.needsService(lastServiceDate), false);
 });
