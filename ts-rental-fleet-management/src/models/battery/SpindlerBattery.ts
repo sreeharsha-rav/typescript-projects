0fa47faa19043016531889a921172a28acf345be
sreeharsha-rav <dev.sreeharsha@gmail.com>
2024-07-22 19:58:29 -0700

    upgraded spindler batteries to service for 3 years

--- ts-rental-fleet-management/src/models/battery/SpindlerBattery.ts
+++ ts-rental-fleet-management/src/models/battery/SpindlerBattery.ts
@@ -7,11 +7,11 @@ export class SpindlerBattery implements Battery {
   /**
    * Determines if the Spindler battery needs service based on the last service date.
    * @param lastServiceDate - The date of the last battery service.
-   * @returns True if the battery needs service (more than 2 years since last service), false otherwise.
+   * @returns True if the battery needs service (more than 3 years since last service), false otherwise.
    */
   needsService(lastServiceDate: Date): boolean {
     const serviceThresholdDate = new Date(lastServiceDate);
-    serviceThresholdDate.setFullYear(serviceThresholdDate.getFullYear() + 2);
+    serviceThresholdDate.setFullYear(serviceThresholdDate.getFullYear() + 3);
     return serviceThresholdDate < new Date();
   }
 }
